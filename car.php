c773ac20c7bc99b20a41cce56edd21197c8689a0
alexcrepy <82715606+alexcrepy@users.noreply.github.com>
2021-11-08 11:51:49 +0100

    Update car.php

--- car.php
+++ car.php
@@ -2,32 +2,16 @@
 
 require_once 'Vehicle.php';
 
-/**
- * Class Car
- */
 class Car extends Vehicle
 {
-    /**
-     * @var string
-     */
+
     private string $energy;
 
-    /**
-     * @var int
-     */
     private int $energyLevel;
 
-    /**
-     * @var boolean
-     */
     private bool $hasParkBrake = false;
 
-    /**
-     * Car constructor.
-     * @param string $color
-     * @param int $nbSeats
-     * @param string $energy
-     */
+
     public function __construct(string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats);
@@ -51,22 +35,15 @@ class Car extends Vehicle
         if ($this->hasParkBrake == true) {
             throw new Exception('Impossible d\'avancer. Veuillez débloquer le frein à main.');
         } else {
-            return "Moteur en marche, frein à main enlever, prêt à partir: Vroouumm Vvvvrrrooouummm!";
+            return "Moteur en marche, frein à main enlever, prêt à partir: Vroouumm Vrooouummm!";
         }
     }
 
-    /**
-     * @return string
-     */
     public function getEnergy(): string
     {
         return $this->energy;
     }
 
-    /**
-     * @param string $energy
-     * @return $this
-     */
     public function setEnergy(string $energy)
     {
         if (in_array($energy, self::ALLOWED_ENERGIES)) {
@@ -75,17 +52,11 @@ class Car extends Vehicle
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getEnergyLevel(): int
     {
         return $this->energyLevel;
     }
 
-    /**
-     * @param int $energyLevel
-     */
     public function setEnergyLevel(int $energyLevel)
     {
         $this->energyLevel = $energyLevel;
